0181a148fb657647855a2ac483ddcc1bbf50fa56
Akshay Mittal <akshay@cs.princeton.edu>
2013-11-22 14:45:42 -0500

    reading the data model in Table to return a new table

--- NoSQL/src/Column.java
+++ NoSQL/src/Column.java
@@ -2,9 +2,23 @@
 public class Column {
 	private Table table;
 	private String name;
+	private Column foreignKeyReference;
+	private boolean isPrimary;
 	
 	Column(Table table, String name) {
 		this.table = table;
 		this.name = name;
+		this.foreignKeyReference = null;
+		this.isPrimary = false;
+	}
+	
+	public Column setForeignKeyReference(Column reference) {
+		this.foreignKeyReference = reference;
+		return this;
+	}
+
+	public Column primary() {
+		this.isPrimary = true;
+		return this;
 	}
 }
--- NoSQL/src/Table.java
+++ NoSQL/src/Table.java
@@ -1,4 +1,8 @@
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.StringTokenizer;
 
 
 public class Table {
@@ -12,5 +16,46 @@ public class Table {
 	
 	public String toString() {
 		return this.name;
+	}
+	
+	public Table addColumn(Column column) {
+		this.columns.add(column);
+		return this;
+	}
+	
+	/**
+	 * Assumes that the data model is in the following format
+	 * 
+	 * table_name
+	 * col1
+	 * col2
+	 * col3 : primary
+	 * .
+	 * .
+	 * .
+	 * coln
+	 * 
+	 * Foreign Key constraints can be added later when needed.
+	 * @param model_path
+	 * @return Table instance for that data model
+	 * @throws FileNotFoundException
+	 */
+	public static Table getTableFromModel(String model_path) throws FileNotFoundException {
+		Scanner ob = new Scanner(new File(model_path));
+		String name = ob.nextLine();
+		Table table = new Table(name);
+		while(ob.hasNextLine()) {
+			String column_info = ob.nextLine();
+			StringTokenizer data = new StringTokenizer(column_info, ": ");
+			Column column = new Column(table, data.nextToken());
+			if (data.hasMoreTokens()) {
+				if (data.nextToken().equals("primary")) {
+					column.primary();
+				}
+			}
+			table.addColumn(column);
+		}
+		ob.close();
+		return table;
 	}
 }
