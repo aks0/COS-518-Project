50ff4560ad27c34caec67be7912d8b2d86dbff05
Sachin Ravi <sachinravi14@gmail.com>
2013-11-22 14:59:12 -0500

    More boilerplate

--- NoSQL/src/Column.java
+++ NoSQL/src/Column.java
@@ -2,9 +2,21 @@
 public class Column {
 	private Table table;
 	private String name;
+	private Column foreignKeyReferencing;
+	private boolean isPrimary;
 	
 	Column(Table table, String name) {
 		this.table = table;
 		this.name = name;
+		foreignKeyReferencing = null;
+		isPrimary = false;
+	}
+
+	public void setPrimary(boolean isPrimary) {
+		this.isPrimary = isPrimary;
+	}
+
+	public void setForeignKeyReferencing(Column foreignKeyFor) {
+		this.foreignKeyReferencing = foreignKeyFor;
 	}
 }
