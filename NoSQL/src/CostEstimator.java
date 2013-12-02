407a3ac2610723e356487d3ce29e89a3ef8c8465
Kevin Lee <kevinlee5@gmail.com>
2013-12-02 03:01:36 -0500

    CostEstimator half of cost based pruning

--- NoSQL/src/CostEstimator.java
+++ NoSQL/src/CostEstimator.java
@@ -4,46 +4,46 @@ import java.util.HashSet;
 
 
 public class CostEstimator {
-	/**
-	 * Normalized cost of all queries which involve subset
-	 * 
-	 * @param subset
-	 * @param queries
-	 * @return
-	 */
-	public static double normalizedCost(TableSubset subset, ArrayList<Query> queries) {
-		double totalCost = 0.0;
-		
-		for (Query query : queries) {
-			boolean subsetRelevant = true;
-			for (Column column : subset.getColumns()) {
-				subsetRelevant = subsetRelevant && query.getReferencedColumns().contains(column);
-				if (!subsetRelevant) {
-					break;
-				}
-			}
-			
-			if (subsetRelevant) {
-				totalCost += normalizedCost(query);
-			}
-		}
-		
-		return totalCost;
-	}
-	
-	/**
-	 * TODO: Discuss a cost model
-	 * Fully normalized cost of query (assuming no indices, denormalization, etc)
-	 * 
-	 * Cost model:
-	 * Charges one unit of cost per cell in the cross join (worst case) of the
-	 * tables referenced by a query where projected columns are those 
-	 * referenced by the query.
-	 * 
-	 * @param query
-	 * @return cost
-	 */
-	public static double normalizedCost(Query query) {
+    /**
+     * Normalized cost of all queries which involve subset
+     * 
+     * @param subset
+     * @param queries
+     * @return
+     */
+    public static double normalizedCost(TableSubset subset, ArrayList<Query> queries) {
+        double totalCost = 0.0;
+        
+        for (Query query : queries) {
+            boolean subsetRelevant = true;
+            for (Column column : subset.getColumns()) {
+                subsetRelevant = subsetRelevant && query.getReferencedColumns().contains(column);
+                if (!subsetRelevant) {
+                    break;
+                }
+            }
+            
+            if (subsetRelevant) {
+                totalCost += normalizedCost(query);
+            }
+        }
+        
+        return totalCost;
+    }
+    
+    /**
+     * TODO: Discuss a cost model
+     * Find normalized cost of query (assuming no indices, denormalization, etc)
+     * 
+     * Cost model:
+     * Charges one unit of cost per cell in the cross join (worst case) of the
+     * tables referenced by a query where projected columns are those 
+     * referenced by the query.
+     * 
+     * @param query
+     * @return cost
+     */
+    public static double normalizedCost(Query query) {
         HashSet<Table> seenTables = new HashSet<Table>();
         
         // get number of cells in cross join of referenced tables with projections
@@ -64,76 +64,98 @@ public class CostEstimator {
         }
         // one unit of cost per cell
         return rows * columns;
-	}
-	
-	/**
-	 * Weighted normalized cost of all queries which involve subset
-	 * 
-	 * @param subset
-	 * @param queries
-	 * @return
-	 */
-	public static double normalizedWeightedCost(TableSubset subset, ArrayList<Query> queries) {
-		double totalCost = 0.0;
-		
-		for (Query query : queries) {
-			boolean subsetRelevant = true;
-			for (Column column : subset.getColumns()) {
-				subsetRelevant = subsetRelevant && query.getReferencedColumns().contains(column);
-				if (!subsetRelevant) {
-					break;
-				}
-			}
-			
-			if (subsetRelevant) {
-				HashSet<Table> tables = new HashSet<Table>();
-				
-				// Get Common tables in query
-				for (Column column : query.getReferencedColumns()) {
-					tables.add(column.getTable());
-				}
-				
-				double totalTablesSize = 0.0;
-				// Add up size of tables
-				for (Table table : tables) {
-					totalTablesSize += table.getSize();
-				}
-				
-				tables.clear();
-				
-				// Get Common tables in table subset
-				for (Column column : subset.getColumns()) {
-					tables.add(column.getTable());
-				}
-				
-				double subsetTablesSize = 0.0;
-				// Add up size of tables
-				for (Table table : tables) {
-					subsetTablesSize += table.getSize();
-				}
-				
-				totalCost += (subsetTablesSize/totalTablesSize) * normalizedCost(query);
-			}
-		}
-		
-		return totalCost;
-	}
-	
-	
-	/**
-	 * TODO: Discuss a cost model
-	 * Fully normalized cost of query (assuming no indices, denormalization, etc)
-	 * 
-	 * Cost model:
-	 * Charges one unit of cost per cell in the cross join (worst case) of the
-	 * tables referenced by a query where projected column are those that 
-	 * are not in the provided table subset.
-	 * 
-	 * @param query
-	 * @param subset
-	 * @return cost
-	 */
-	public static double denormalizedCost(Query query, TableSubset subset) {
+    }
+    
+    /**
+     * Weighted normalized cost of all queries which involve subset
+     * 
+     * @param subset
+     * @param queries
+     * @return
+     */
+    public static double normalizedWeightedCost(TableSubset subset, ArrayList<Query> queries) {
+        double totalCost = 0.0;
+        
+        for (Query query : queries) {
+            boolean subsetRelevant = true;
+            for (Column column : subset.getColumns()) {
+                subsetRelevant = subsetRelevant && query.getReferencedColumns().contains(column);
+                if (!subsetRelevant) {
+                    break;
+                }
+            }
+            
+            if (subsetRelevant) {
+                HashSet<Table> tables = new HashSet<Table>();
+                
+                // Get Common tables in query
+                for (Column column : query.getReferencedColumns()) {
+                    tables.add(column.getTable());
+                }
+                
+                double totalTablesSize = 0.0;
+                // Add up size of tables
+                for (Table table : tables) {
+                    totalTablesSize += table.getSize();
+                }
+                
+                tables.clear();
+                
+                // Get Common tables in table subset
+                for (Column column : subset.getColumns()) {
+                    tables.add(column.getTable());
+                }
+                
+                double subsetTablesSize = 0.0;
+                // Add up size of tables
+                for (Table table : tables) {
+                    subsetTablesSize += table.getSize();
+                }
+                
+                totalCost += (subsetTablesSize/totalTablesSize) * normalizedCost(query);
+            }
+        }
+        
+        return totalCost;
+    }
+    
+    /**
+     * Cost based pruner
+     * Selects the best table subset from a provided list of subsets proposed for the given query
+     * based on minimum denormalized cost
+     * 
+     * @param query
+     * @param proposedSubsets
+     * @return best subset
+     */
+    public static TableSubset findBestSubset(Query query, ArrayList<TableSubset> proposedSubsets) {
+        double minCost = Double.POSITIVE_INFINITY;
+        TableSubset bestSubset = null;
+        for (TableSubset subset : proposedSubsets) {
+            double cost = denormalizedCost(query, subset);
+            if (cost < minCost) {
+                minCost = cost;
+                bestSubset = subset;
+            }
+        }
+        return bestSubset;
+    }
+    
+    /**
+     * TODO: Discuss a cost model
+     * Helper function to findBestSubset()
+     * Find denormalized cost of query with respect to provided table subset
+     * 
+     * Cost model:
+     * Charges one unit of cost per cell in the cross join (worst case) of the
+     * tables referenced by a query where projected column are those that 
+     * are not in the provided table subset.
+     * 
+     * @param query
+     * @param subset
+     * @return cost
+     */
+    private static double denormalizedCost(Query query, TableSubset subset) {
         HashSet<Table> seenTables = new HashSet<Table>();
 
         // get number of cells in cross join of referenced tables with projections
@@ -158,7 +180,7 @@ public class CostEstimator {
             }
         }
         // one unit of cost per cell
-        // may also subtract a multiple of size of subset (as maintenance/storage cost) here
+        // may also add a multiple of size of subset (as maintenance/storage cost) here
         return rows * columns;
-	}
+    }
 }
