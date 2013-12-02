5e512f232a23417c6acc65d5dbde04291fe9f257
Kevin Lee <kevinlee5@gmail.com>
2013-12-02 01:59:53 -0500

    First proposed cost model

--- NoSQL/src/CostEstimator.java
+++ NoSQL/src/CostEstimator.java
@@ -32,13 +32,38 @@ public class CostEstimator {
 	}
 	
 	/**
-	 * TODO: Write this - fully normalized cost of query (assuming no indices, denormalization, etc)
+	 * TODO: Discuss a cost model
+	 * Fully normalized cost of query (assuming no indices, denormalization, etc)
+	 * 
+	 * Cost model:
+	 * Charges one unit of cost per cell in the cross join (worst case) of the
+	 * tables referenced by a query where projected columns are those 
+	 * referenced by the query.
 	 * 
 	 * @param query
-	 * @return
+	 * @return cost
 	 */
 	public static double normalizedCost(Query query) {
-	    return 0.0;
+        HashSet<Table> seenTables = new HashSet<Table>();
+        
+        // get number of cells in cross join of referenced tables with projections
+        int columns = query.getReferencedColumns().size();
+        int rows = 0;
+        for (Column column : query.getReferencedColumns()) {
+            Table referencedTable = column.getTable();
+            if (!seenTables.contains(referencedTable)) {
+                seenTables.add(referencedTable);
+                if (rows == 0) {
+                    // first table to fetch
+                    rows = referencedTable.getSize();
+                } else {
+                    // scan through next table for joins
+                    rows *= referencedTable.getSize();
+                }
+            }
+        }
+        // one unit of cost per cell
+        return rows * columns;
 	}
 	
 	/**
@@ -92,5 +117,48 @@ public class CostEstimator {
 		}
 		
 		return totalCost;
+	}
+	
+	
+	/**
+	 * TODO: Discuss a cost model
+	 * Fully normalized cost of query (assuming no indices, denormalization, etc)
+	 * 
+	 * Cost model:
+	 * Charges one unit of cost per cell in the cross join (worst case) of the
+	 * tables referenced by a query where projected column are those that 
+	 * are not in the provided table subset.
+	 * 
+	 * @param query
+	 * @param subset
+	 * @return cost
+	 */
+	public static double denormalizedCost(Query query, TableSubset subset) {
+        HashSet<Table> seenTables = new HashSet<Table>();
+
+        // get number of cells in cross join of referenced tables with projections
+        // omit projection of columns that are in subset
+        int columns = 0;
+        int rows = 0;
+        for (Column column : query.getReferencedColumns()) {
+            // project column if it is not in subset
+            if (!subset.getColumns().contains(column)) {
+                columns++;
+                Table referencedTable = column.getTable();
+                if (!seenTables.contains(referencedTable)) {
+                    seenTables.add(referencedTable);
+                    if (rows == 0) {
+                        // first table to fetch
+                        rows = referencedTable.getSize();
+                    } else {
+                        // scan through next table for joins
+                        rows *= referencedTable.getSize();
+                    }
+                }
+            }
+        }
+        // one unit of cost per cell
+        // may also subtract a multiple of size of subset (as maintenance/storage cost) here
+        return rows * columns;
 	}
 }
