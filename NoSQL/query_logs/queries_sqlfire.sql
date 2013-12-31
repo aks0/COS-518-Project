dd1f2da0a4382409269c90de0166d2ea5ad728dd
Sachin Ravi <sachinravi14@gmail.com>
2013-12-31 18:06:54 -0500

    Query logs and data model for SQL Fire example

---
+++ NoSQL/query_logs/queries_sqlfire.sql
@@ -0,0 +1,47 @@
+SELECT Customers.ContactName 
+FROM Customers, Orders 
+WHERE Customers.CustomerID = Orders.CustomerID;
+
+SELECT Customers.ContactTitle 
+FROM Customers, Orders 
+WHERE Customers.CustomerID = Orders.CustomerID;
+
+SELECT Customers.Address 
+FROM Customers, Orders 
+WHERE Customers.CustomerID = Orders.CustomerID;
+
+SELECT Customers.Region 
+FROM Customers, Orders 
+WHERE Customers.CustomerID = Orders.CustomerID;
+
+SELECT Customers.PostalCode 
+FROM Customers, Orders 
+WHERE Customers.CustomerID = Orders.CustomerID;
+
+SELECT Customers.ContactName 
+FROM Customers, Orders 
+WHERE Customers.CustomerID = Orders.CustomerID;
+
+SELECT Customers.ContactTitle 
+FROM Customers, Orders 
+WHERE Customers.CustomerID = Orders.CustomerID;
+
+SELECT Customers.Address 
+FROM Customers, Orders 
+WHERE Customers.CustomerID = Orders.CustomerID;
+
+SELECT Customers.Region 
+FROM Customers, Orders 
+WHERE Customers.CustomerID = Orders.CustomerID;
+
+SELECT Customers.PostalCode 
+FROM Customers, Orders 
+WHERE Customers.CustomerID = Orders.CustomerID;
+
+SELECT Employees.LastName 
+FROM Employees, Orders 
+WHERE Employees.EmployeeID = Orders.EmployeeID;
+
+SELECT Employees.FirstName 
+FROM Employees, Orders 
+WHERE Employees.EmployeeID = Orders.EmployeeID;
