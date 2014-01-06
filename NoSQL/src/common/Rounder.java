3ddd28e901e5ce79f88b09f2a9984a19cb9151c7
Sachin Ravi <sachinravi14@gmail.com>
2014-01-05 19:44:24 -0500

    Class to round memory size

---
+++ NoSQL/src/common/Rounder.java
@@ -0,0 +1,13 @@
+package common;
+
+import java.math.BigDecimal;
+
+public class Rounder {
+	public static double round(double value, int places) {
+	    if (places < 0) throw new IllegalArgumentException();
+
+	    BigDecimal bd = new BigDecimal(value);
+	    bd = bd.setScale(places, BigDecimal.ROUND_HALF_UP);
+	    return bd.doubleValue();
+	}
+}
