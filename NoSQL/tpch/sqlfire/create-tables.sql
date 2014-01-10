43ab5aa00405bfe43ce5395ebf052d8f033be855
Akshay Mittal <akshay@cs.princeton.edu>
2014-01-10 14:51:57 -0500

    small tweaks

--- NoSQL/tpch/sqlfire/create-tables.sql
+++ NoSQL/tpch/sqlfire/create-tables.sql
@@ -1,69 +1,69 @@
-CREATE TABLE NATION  ( N_NATIONKEY  INTEGER NOT NULL,
-                            N_NAME       CHAR(25) NOT NULL,
-                            N_REGIONKEY  INTEGER NOT NULL,
-                            N_COMMENT    VARCHAR(152));
+CREATE TABLE NATION  ( NATIONKEY  INTEGER NOT NULL,
+                            NAME       CHAR(25) NOT NULL,
+                            REGIONKEY  INTEGER NOT NULL,
+                            COMMENT    VARCHAR(152));
 
-CREATE TABLE REGION  ( R_REGIONKEY  INTEGER NOT NULL,
-                            R_NAME       CHAR(25) NOT NULL,
-                            R_COMMENT    VARCHAR(152));
+CREATE TABLE REGION  ( REGIONKEY  INTEGER NOT NULL,
+                            NAME       CHAR(25) NOT NULL,
+                            COMMENT    VARCHAR(152));
 
-CREATE TABLE PART  ( P_PARTKEY     INTEGER NOT NULL,
-                          P_NAME        VARCHAR(55) NOT NULL,
-                          P_MFGR        CHAR(25) NOT NULL,
-                          P_BRAND       CHAR(10) NOT NULL,
-                          P_TYPE        VARCHAR(25) NOT NULL,
-                          P_SIZE        INTEGER NOT NULL,
-                          P_CONTAINER   CHAR(10) NOT NULL,
-                          P_RETAILPRICE DECIMAL(15,2) NOT NULL,
-                          P_COMMENT     VARCHAR(23) NOT NULL );
+CREATE TABLE PART  ( PARTKEY     INTEGER NOT NULL,
+                          NAME        VARCHAR(55) NOT NULL,
+                          MFGR        CHAR(25) NOT NULL,
+                          BRAND       CHAR(10) NOT NULL,
+                          TYPE        VARCHAR(25) NOT NULL,
+                          SIZE        INTEGER NOT NULL,
+                          CONTAINER   CHAR(10) NOT NULL,
+                          RETAILPRICE DECIMAL(15,2) NOT NULL,
+                          COMMENT     VARCHAR(23) NOT NULL );
 
-CREATE TABLE SUPPLIER ( S_SUPPKEY     INTEGER NOT NULL,
-                             S_NAME        CHAR(25) NOT NULL,
-                             S_ADDRESS     VARCHAR(40) NOT NULL,
-                             S_NATIONKEY   INTEGER NOT NULL,
-                             S_PHONE       CHAR(15) NOT NULL,
-                             S_ACCTBAL     DECIMAL(15,2) NOT NULL,
-                             S_COMMENT     VARCHAR(101) NOT NULL);
+CREATE TABLE SUPPLIER ( SUPPKEY     INTEGER NOT NULL,
+                             NAME        CHAR(25) NOT NULL,
+                             ADDRESS     VARCHAR(40) NOT NULL,
+                             NATIONKEY   INTEGER NOT NULL,
+                             PHONE       CHAR(15) NOT NULL,
+                             ACCTBAL     DECIMAL(15,2) NOT NULL,
+                             COMMENT     VARCHAR(101) NOT NULL);
 
-CREATE TABLE PARTSUPP ( PS_PARTKEY     INTEGER NOT NULL,
-                             PS_SUPPKEY     INTEGER NOT NULL,
-                             PS_AVAILQTY    INTEGER NOT NULL,
-                             PS_SUPPLYCOST  DECIMAL(15,2)  NOT NULL,
-                             PS_COMMENT     VARCHAR(199) NOT NULL );
+CREATE TABLE PARTSUPP ( PARTKEY     INTEGER NOT NULL,
+                             SUPPKEY     INTEGER NOT NULL,
+                             AVAILQTY    INTEGER NOT NULL,
+                             SUPPLYCOST  DECIMAL(15,2)  NOT NULL,
+                             COMMENT     VARCHAR(199) NOT NULL );
 
-CREATE TABLE CUSTOMER ( C_CUSTKEY     INTEGER NOT NULL,
-                             C_NAME        VARCHAR(25) NOT NULL,
-                             C_ADDRESS     VARCHAR(40) NOT NULL,
-                             C_NATIONKEY   INTEGER NOT NULL,
-                             C_PHONE       CHAR(15) NOT NULL,
-                             C_ACCTBAL     DECIMAL(15,2)   NOT NULL,
-                             C_MKTSEGMENT  CHAR(10) NOT NULL,
-                             C_COMMENT     VARCHAR(117) NOT NULL);
+CREATE TABLE CUSTOMER ( CUSTKEY     INTEGER NOT NULL,
+                             NAME        VARCHAR(25) NOT NULL,
+                             ADDRESS     VARCHAR(40) NOT NULL,
+                             NATIONKEY   INTEGER NOT NULL,
+                             PHONE       CHAR(15) NOT NULL,
+                             ACCTBAL     DECIMAL(15,2)   NOT NULL,
+                             MKTSEGMENT  CHAR(10) NOT NULL,
+                             COMMENT     VARCHAR(117) NOT NULL);
 
-CREATE TABLE ORDERS  ( O_ORDERKEY       INTEGER NOT NULL,
-                           O_CUSTKEY        INTEGER NOT NULL,
-                           O_ORDERSTATUS    CHAR(1) NOT NULL,
-                           O_TOTALPRICE     DECIMAL(15,2) NOT NULL,
-                           O_ORDERDATE      DATE NOT NULL,
-                           O_ORDERPRIORITY  CHAR(15) NOT NULL,  -- R
-                           O_CLERK          CHAR(15) NOT NULL,  -- R
-                           O_SHIPPRIORITY   INTEGER NOT NULL,
-                           O_COMMENT        VARCHAR(79) NOT NULL);
+CREATE TABLE ORDERS  ( ORDERKEY       INTEGER NOT NULL,
+                           CUSTKEY        INTEGER NOT NULL,
+                           ORDERSTATUS    CHAR(1) NOT NULL,
+                           TOTALPRICE     DECIMAL(15,2) NOT NULL,
+                           ORDERDATE      DATE NOT NULL,
+                           ORDERPRIORITY  CHAR(15) NOT NULL,  -- R
+                           CLERK          CHAR(15) NOT NULL,  -- R
+                           SHIPPRIORITY   INTEGER NOT NULL,
+                           COMMENT        VARCHAR(79) NOT NULL);
 
-CREATE TABLE LINEITEM ( L_ORDERKEY    INTEGER NOT NULL,
-                             L_PARTKEY     INTEGER NOT NULL,
-                             L_SUPPKEY     INTEGER NOT NULL,
-                             L_LINENUMBER  INTEGER NOT NULL,
-                             L_QUANTITY    DECIMAL(15,2) NOT NULL,
-                             L_EXTENDEDPRICE  DECIMAL(15,2) NOT NULL,
-                             L_DISCOUNT    DECIMAL(15,2) NOT NULL,
-                             L_TAX         DECIMAL(15,2) NOT NULL,
-                             L_RETURNFLAG  CHAR(1) NOT NULL,
-                             L_LINESTATUS  CHAR(1) NOT NULL,
-                             L_SHIPDATE    DATE NOT NULL,
-                             L_COMMITDATE  DATE NOT NULL,
-                             L_RECEIPTDATE DATE NOT NULL,
-                             L_SHIPINSTRUCT CHAR(25) NOT NULL,  -- R
-                             L_SHIPMODE     CHAR(10) NOT NULL,  -- R
-                             L_COMMENT      VARCHAR(44) NOT NULL);
+CREATE TABLE LINEITEM ( ORDERKEY    INTEGER NOT NULL,
+                             PARTKEY     INTEGER NOT NULL,
+                             SUPPKEY     INTEGER NOT NULL,
+                             LINENUMBER  INTEGER NOT NULL,
+                             QUANTITY    DECIMAL(15,2) NOT NULL,
+                             EXTENDEDPRICE  DECIMAL(15,2) NOT NULL,
+                             DISCOUNT    DECIMAL(15,2) NOT NULL,
+                             TAX         DECIMAL(15,2) NOT NULL,
+                             RETURNFLAG  CHAR(1) NOT NULL,
+                             LINESTATUS  CHAR(1) NOT NULL,
+                             SHIPDATE    DATE NOT NULL,
+                             COMMITDATE  DATE NOT NULL,
+                             RECEIPTDATE DATE NOT NULL,
+                             SHIPINSTRUCT CHAR(25) NOT NULL,  -- R
+                             SHIPMODE     CHAR(10) NOT NULL,  -- R
+                             COMMENT      VARCHAR(44) NOT NULL);
 
