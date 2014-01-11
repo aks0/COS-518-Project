4b491db858af67cf46d357d6677a4a6bc4fe2b18
Kevin Lee <klee5@node0.sqlfire.astream561.emulab.net>
2014-01-11 00:34:52 -0700

    scripts for compiling and running easily

---
+++ NoSQL/compile.sh
@@ -0,0 +1,1 @@
+javac -d bin -cp gsp.jar src/colocation/*.java src/gudusoft/gsqlparser/*.java src/sqlfire/*.java src/common/*.java src/materializedViews/*.java
---
+++ NoSQL/run.sh
@@ -0,0 +1,1 @@
+java -cp bin:gsp.jar:sqlfireclient.jar $1
