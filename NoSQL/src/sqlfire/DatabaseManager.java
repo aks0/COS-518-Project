1d15febdaa9083e35979fbf7e36f049cc3ec6dfa
Kevin Lee <kevinlee5@gmail.com>
2014-01-12 17:28:19 -0500

    fixing join bug

--- NoSQL/src/sqlfire/DatabaseManager.java
+++ NoSQL/src/sqlfire/DatabaseManager.java
@@ -162,8 +162,10 @@ public class DatabaseManager {
                 System.out.println(table1.getName() + "X" + table2.getName() + " joined");
                 
                 // fetch both tables
-                String selectQuery1 = constructSelectQuery(table1, query.getTableToColumns().get(table1));
-                String selectQuery2 = constructSelectQuery(table2, query.getTableToColumns().get(table2));
+                String selectQuery1 = constructSelectQuery(table1, column1, query.getTableToColumns().get(table1));
+                System.out.println(selectQuery1);
+                String selectQuery2 = constructSelectQuery(table2, column2, query.getTableToColumns().get(table2));
+                System.out.println(selectQuery2);
                 
                 try{
                     CachedRowSet tableResult1 = new CachedRowSetImpl();
@@ -229,14 +231,14 @@ public class DatabaseManager {
         return query;
     }
     
-    private String constructSelectQuery(Table table, HashSet<Column> columns) {
-        String query = "select ";
+    private String constructSelectQuery(Table table, Column requiredColumn, HashSet<Column> columns) {
+        String query = "select " + requiredColumn.getName();
         int columnNum = 1;
         for (Column column : columns) {
-            query += column.getName();
             if (columnNum++ != columns.size()) {
                 query += ", ";
             }
+            query += column.getName();
         }
         query += " from " + table.getName() + ";"; 
         return query;
