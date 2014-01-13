7553c020a53c31ba215205e48d011f580de9f95c
Sachin Ravi <sachinravi14@gmail.com>
2014-01-12 20:29:55 -0500

    moving non-modified degree sql file

---
+++ NoSQL/query_logs/queries-degree-non-modified.sql
@@ -0,0 +1,688 @@
+
+
+select
+    o.orderdate,
+    l.extendedprice,
+    l.discount,
+    n2.name
+from
+    part p,
+    supplier s,
+    lineitem l,
+    orders o,
+    customer c,
+    nation n1,
+    nation n2,
+    region r
+where
+    p.partkey = l.partkey
+    and s.suppkey = l.suppkey
+    and l.orderkey = o.orderkey
+    and o.custkey = c.custkey
+    and c.nationkey = n1.nationkey
+    and n1.regionkey = r.regionkey;
+
+
+select
+	l.extendedprice
+from
+	lineitem l,
+	part p
+where
+	p.partkey = l.partkey;
+
+
+select
+    o.orderdate,
+    l.extendedprice,
+    l.discount,
+    n2.name
+from
+    part p,
+    supplier s,
+    lineitem l,
+    orders o,
+    customer c,
+    nation n1,
+    nation n2,
+    region r
+where
+    p.partkey = l.partkey
+    and s.suppkey = l.suppkey
+    and l.orderkey = o.orderkey
+    and o.custkey = c.custkey
+    and c.nationkey = n1.nationkey
+    and n1.regionkey = r.regionkey;
+
+
+select
+    o.orderdate,
+    l.extendedprice,
+    l.discount,
+    n2.name
+from
+    part p,
+    supplier s,
+    lineitem l,
+    orders o,
+    customer c,
+    nation n1,
+    nation n2,
+    region r
+where
+    p.partkey = l.partkey
+    and s.suppkey = l.suppkey
+    and l.orderkey = o.orderkey
+    and o.custkey = c.custkey
+    and c.nationkey = n1.nationkey
+    and n1.regionkey = r.regionkey;
+
+
+
+select
+	n.name,
+	l.extendedprice,
+	l.discount
+from
+	customer c,
+	orders o,
+	lineitem l,
+	supplier s,
+	nation n,
+	region r
+where
+	c.custkey = o.custkey
+	and l.orderkey = o.orderkey
+	and l.suppkey = s.suppkey
+	and c.nationkey = s.nationkey
+	and s.nationkey = n.nationkey
+	and n.regionkey = r.regionkey;
+
+
+select
+    o.orderdate,
+    l.extendedprice,
+    l.discount,
+    n2.name
+from
+    part p,
+    supplier s,
+    lineitem l,
+    orders o,
+    customer c,
+    nation n1,
+    nation n2,
+    region r
+where
+    p.partkey = l.partkey
+    and s.suppkey = l.suppkey
+    and l.orderkey = o.orderkey
+    and o.custkey = c.custkey
+    and c.nationkey = n1.nationkey
+    and n1.regionkey = r.regionkey;
+
+
+select
+	l.extendedprice
+from
+	lineitem l,
+	part p
+where
+	p.partkey = l.partkey;
+
+
+select
+	l.extendedprice
+from
+	lineitem l,
+	part p
+where
+	p.partkey = l.partkey;
+
+
+select
+	p.type,
+	l.extendedprice,
+	l.discount
+from
+	lineitem l,
+	part p
+where
+	l.partkey = p.partkey;
+
+
+select
+	l.extendedprice,
+	l.discount
+from
+	lineitem l,
+	part p
+where
+	p.partkey = l.partkey;
+
+
+select
+	l.extendedprice,
+	l.discount
+from
+	lineitem l;
+
+
+select
+	p.type,
+	l.extendedprice,
+	l.discount
+from
+	lineitem l,
+	part p
+where
+	l.partkey = p.partkey;
+
+
+select
+	l.extendedprice,
+	l.discount
+from
+	lineitem l,
+	part p
+where
+	p.partkey = l.partkey;
+
+
+select
+    o.orderdate,
+    l.extendedprice,
+    l.discount,
+    n2.name
+from
+    part p,
+    supplier s,
+    lineitem l,
+    orders o,
+    customer c,
+    nation n1,
+    nation n2,
+    region r
+where
+    p.partkey = l.partkey
+    and s.suppkey = l.suppkey
+    and l.orderkey = o.orderkey
+    and o.custkey = c.custkey
+    and c.nationkey = n1.nationkey
+    and n1.regionkey = r.regionkey;
+
+
+select
+	l.extendedprice,
+	l.discount
+from
+	lineitem l,
+	part p
+where
+	p.partkey = l.partkey;
+
+
+select
+	l.extendedprice
+from
+	lineitem l,
+	part p
+where
+	p.partkey = l.partkey;
+
+
+select
+	l.extendedprice,
+	l.discount
+from
+	lineitem l,
+	part p
+where
+	p.partkey = l.partkey;
+
+
+select
+    c.custkey,
+    c.name,
+    l.extendedprice,
+    l.discount,
+    c.acctbal,
+    n.name,
+    c.address,
+    c.phone,
+    c.comment
+from
+	customer c,
+	orders o,
+	lineitem l,
+	nation n
+where
+	c.custkey = o.custkey
+	and l.orderkey = o.orderkey
+	and c.nationkey = n.nationkey;
+
+
+select
+	s.acctbal,
+	s.name,
+	n.name,
+	p.partkey,
+	p.mfgr,
+	s.address,
+	s.phone,
+	s.comment
+from
+	part p,
+	supplier s,
+	partsupp ps,
+	nation n,
+	region r
+where
+	p.partkey = ps.partkey
+	and s.suppkey = ps.suppkey;
+
+
+select
+	l.extendedprice
+from
+	lineitem l,
+	part p
+where
+	p.partkey = l.partkey;
+
+
+select
+	l.extendedprice,
+	l.discount
+from
+	lineitem l,
+	part p
+where
+	p.partkey = l.partkey;
+
+
+select
+	l.extendedprice,
+	l.discount
+from
+	lineitem l,
+	part p
+where
+	p.partkey = l.partkey;
+
+
+select
+	p.type,
+	l.extendedprice,
+	l.discount
+from
+	lineitem l,
+	part p
+where
+	l.partkey = p.partkey;
+
+
+select
+	l.extendedprice,
+	l.discount
+from
+	lineitem l,
+	part p
+where
+	p.partkey = l.partkey;
+
+
+select
+	s.acctbal,
+	s.name,
+	n.name,
+	p.partkey,
+	p.mfgr,
+	s.address,
+	s.phone,
+	s.comment
+from
+	part p,
+	supplier s,
+	partsupp ps,
+	nation n,
+	region r
+where
+	p.partkey = ps.partkey
+	and s.suppkey = ps.suppkey;
+
+
+select
+    o.orderdate,
+    l.extendedprice,
+    l.discount,
+    n2.name
+from
+    part p,
+    supplier s,
+    lineitem l,
+    orders o,
+    customer c,
+    nation n1,
+    nation n2,
+    region r
+where
+    p.partkey = l.partkey
+    and s.suppkey = l.suppkey
+    and l.orderkey = o.orderkey
+    and o.custkey = c.custkey
+    and c.nationkey = n1.nationkey
+    and n1.regionkey = r.regionkey;
+
+
+select
+	l.extendedprice,
+	l.discount
+from
+	lineitem l,
+	part p
+where
+	p.partkey = l.partkey;
+
+
+select
+	p.type,
+	l.extendedprice,
+	l.discount
+from
+	lineitem l,
+	part p
+where
+	l.partkey = p.partkey;
+
+
+select
+    o.orderdate,
+    l.extendedprice,
+    l.discount,
+    n2.name
+from
+    part p,
+    supplier s,
+    lineitem l,
+    orders o,
+    customer c,
+    nation n1,
+    nation n2,
+    region r
+where
+    p.partkey = l.partkey
+    and s.suppkey = l.suppkey
+    and l.orderkey = o.orderkey
+    and o.custkey = c.custkey
+    and c.nationkey = n1.nationkey
+    and n1.regionkey = r.regionkey;
+
+
+select
+	l.extendedprice
+from
+	lineitem l,
+	part p
+where
+	p.partkey = l.partkey;
+
+
+select
+	l.extendedprice,
+	l.discount
+from
+	lineitem l;
+
+
+select
+	p.type,
+	l.extendedprice,
+	l.discount
+from
+	lineitem l,
+	part p
+where
+	l.partkey = p.partkey;
+
+
+select
+	l.extendedprice,
+	l.discount
+from
+	lineitem l,
+	part p
+where
+	p.partkey = l.partkey;
+
+
+select
+	s.name
+from
+	supplier s,
+	lineitem l1,
+	orders o,
+	nation n
+where
+	s.suppkey = l1.suppkey
+	and o.orderkey = l1.orderkey;
+
+
+select
+	l.extendedprice,
+	l.discount
+from
+	lineitem l,
+	part p
+where
+	p.partkey = l.partkey;
+
+
+select
+	l.extendedprice,
+	l.discount
+from
+	lineitem l,
+	part p
+where
+	p.partkey = l.partkey;
+
+
+select
+	l.extendedprice
+from
+	lineitem l,
+	part p
+where
+	p.partkey = l.partkey;
+
+
+select
+    n1.name,
+    n2.name,
+    l.shipdate,
+    l.extendedprice,
+    l.discount
+from
+    supplier s,
+    lineitem l ,
+    orders o,
+    customer c,
+    nation n1,
+    nation n2
+where
+    s.suppkey = l.suppkey
+    and o.orderkey = l.orderkey
+    and c.custkey = o.custkey
+    and s.nationkey = n1.nationkey
+    and c.nationkey = n2.nationkey;
+
+
+select
+	s.name,
+	s.address
+from
+	supplier s,
+	nation n
+where
+    s.nationkey = n.nationkey;
+
+
+select
+    o.orderdate,
+    l.extendedprice,
+    l.discount,
+    n2.name
+from
+    part p,
+    supplier s,
+    lineitem l,
+    orders o,
+    customer c,
+    nation n1,
+    nation n2,
+    region r
+where
+    p.partkey = l.partkey
+    and s.suppkey = l.suppkey
+    and l.orderkey = o.orderkey
+    and o.custkey = c.custkey
+    and c.nationkey = n1.nationkey
+    and n1.regionkey = r.regionkey;
+
+
+select
+	l.extendedprice,
+	l.discount
+from
+	lineitem l,
+	part p
+where
+	p.partkey = l.partkey;
+
+
+select
+	l.extendedprice,
+	l.discount
+from
+	lineitem l,
+	part p
+where
+	p.partkey = l.partkey;
+
+
+select
+	l.extendedprice,
+	l.discount
+from
+	lineitem l,
+	part p
+where
+	p.partkey = l.partkey;
+
+
+select
+	p.type,
+	l.extendedprice,
+	l.discount
+from
+	lineitem l,
+	part p
+where
+	l.partkey = p.partkey;
+
+
+select
+	l.extendedprice,
+	l.discount
+from
+	lineitem l,
+	part p
+where
+	p.partkey = l.partkey;
+
+
+select
+    o.orderdate,
+    l.extendedprice,
+    l.discount,
+    n2.name
+from
+    part p,
+    supplier s,
+    lineitem l,
+    orders o,
+    customer c,
+    nation n1,
+    nation n2,
+    region r
+where
+    p.partkey = l.partkey
+    and s.suppkey = l.suppkey
+    and l.orderkey = o.orderkey
+    and o.custkey = c.custkey
+    and c.nationkey = n1.nationkey
+    and n1.regionkey = r.regionkey;
+
+
+select
+	s.name
+from
+	supplier s,
+	lineitem l1,
+	orders o,
+	nation n
+where
+	s.suppkey = l1.suppkey
+	and o.orderkey = l1.orderkey;
+
+
+select
+    n1.name,
+    n2.name,
+    l.shipdate,
+    l.extendedprice,
+    l.discount
+from
+    supplier s,
+    lineitem l ,
+    orders o,
+    customer c,
+    nation n1,
+    nation n2
+where
+    s.suppkey = l.suppkey
+    and o.orderkey = l.orderkey
+    and c.custkey = o.custkey
+    and s.nationkey = n1.nationkey
+    and c.nationkey = n2.nationkey;
+
+
+select
+	s.acctbal,
+	s.name,
+	n.name,
+	p.partkey,
+	p.mfgr,
+	s.address,
+	s.phone,
+	s.comment
+from
+	part p,
+	supplier s,
+	partsupp ps,
+	nation n,
+	region r
+where
+	p.partkey = ps.partkey
+	and s.suppkey = ps.suppkey;
+
+
+select
+	l.extendedprice,
+	l.discount
+from
+	lineitem l,
+	part p
+where
+	p.partkey = l.partkey;
